8ac650ad5bb28fc83fa23084c782f5ad6b9cc41d
LeoCiru <vasquezvolquezfelixleonel@gmail.com>
2026-08-12 15:22:16 -0400

    Registrar campos del formulario de registro en React Hook Form

--- src/features/auth/components/RegisterForm.tsx
+++ src/features/auth/components/RegisterForm.tsx
@@ -1,19 +1,22 @@
 import { Form,FormInput,FormLabel, FormSubmit } from "@/src/shared/components/forms";
+import { useForm } from "react-hook-form";
 
 export default function RegisterForm() {
+
+  const { register,  } = useForm()
   return (
     <Form>
         <FormLabel htmlFor="name">Nombre</FormLabel>
-        <FormInput type="text" id="name" placeholder="Ingresa tu nombre"/>
+        <FormInput type="text" id="name" placeholder="Ingresa tu nombre" {...register("name")} />
 
         <FormLabel htmlFor="email">Correo Electrónico</FormLabel>
-        <FormInput type="email" id="email" placeholder="Ingresa tu correo electrónico"/>
+        <FormInput type="email" id="email" placeholder="Ingresa tu correo electrónico" { ...register("email") }/>
 
         <FormLabel htmlFor="password">Contraseña</FormLabel>
-        <FormInput type="password" id="password" placeholder="Contraseña - Min. 8 caracteres"/>
+        <FormInput type="password" id="password" placeholder="Contraseña - Min. 8 caracteres" { ...register("password") }/>
 
         <FormLabel htmlFor="password_confirmation">Repetir contraseña</FormLabel>
-        <FormInput type="password" id="password_confirmation" placeholder="Repite tu contraseña"/>
+        <FormInput type="password" id="password_confirmation" placeholder="Repite tu contraseña" { ...register("passwordConfirmation") } />
 
         <FormSubmit value="Registrarme"/>
     </Form>
