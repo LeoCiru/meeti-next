71159b747ec26c0e5955fc333fbc4376d8d46411
LeoCiru <vasquezvolquezfelixleonel@gmail.com>
2026-08-12 13:41:18 -0400

    Crear componente reutilizable para submit

--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -3,6 +3,7 @@
 import Form from "@/src/shared/components/forms/Form"
 import FormInput from "@/src/shared/components/forms/FormInput"
 import FormLabel from "@/src/shared/components/forms/FormLabel"
+import FormSubmit from "@/src/shared/components/forms/FormSubmit"
 
 
 export default function LoginForm() {
@@ -13,6 +14,8 @@ export default function LoginForm() {
 
         <FormLabel htmlFor="password">Contraseña</FormLabel>
         <FormInput type="password" id="password" placeholder="Ingresa tu email" />
+
+        <FormSubmit value="Iniciar Sesión" />
     </Form>
   )
 }
---
+++ src/shared/components/forms/FormSubmit.tsx
@@ -0,0 +1,11 @@
+import { HtmlHTMLAttributes, InputHTMLAttributes } from "react";
+
+type Props = InputHTMLAttributes<HTMLInputElement>
+
+export default function FormSubmit(props : Props) {
+
+    const { value } = props;
+  return (
+    <input {...props} type="submit" className="bg-pink-600 w-full p-2 uppercase font-black text-white cursor-pointer mt-5" />
+  )
+}
