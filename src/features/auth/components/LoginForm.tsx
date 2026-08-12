bba38bad4665ff6273775c78cdc03f51c7ce7874
LeoCiru <vasquezvolquezfelixleonel@gmail.com>
2026-08-12 11:35:43 -0400

    Crear componente reutilizable para inputs

--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import Form from "@/src/shared/components/forms/Form"
+import FormInput from "@/src/shared/components/forms/FormInput"
 import FormLabel from "@/src/shared/components/forms/FormLabel"
 
 
@@ -8,10 +9,10 @@ export default function LoginForm() {
   return (
     <Form>
         <FormLabel htmlFor="email">Email</FormLabel>
-        <input type="email" id="email" placeholder="Ingresa tu email" className="border border-slate-200 w-full p-2"/>
+        <FormInput type="email" id="email" placeholder="Ingresa tu email" />
 
         <FormLabel htmlFor="password">Contraseña</FormLabel>
-        <input type="password" id="password" placeholder="Ingresa tu contraseña" className="border border-slate-200 w-full p-2"/>
+        <FormInput type="password" id="password" placeholder="Ingresa tu email" />
     </Form>
   )
 }
