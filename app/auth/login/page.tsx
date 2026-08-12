b3e06d88930e36e7d2c89b7e85b0b8700691b329
LeoCiru <vasquezvolquezfelixleonel@gmail.com>
2026-08-12 10:52:12 -0400

    Permitir componente Form recibir props de etiqueta

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import Heading from "@/src/shared/components/typography/Heading";
 import { generatePageTitle } from "@/src/shared/utils/metadata";
+import LoginForm from "@/src/features/auth/components/LoginForm";
 
 export const metadata: Metadata = {
   title: generatePageTitle("Iniciar Sesión"),
@@ -10,6 +11,8 @@ export default function LoginPage() {
   return (
     <>
       <Heading>Iniciar Sesión</Heading>
+
+      <LoginForm />
     </>
   )
 }
---
+++ src/features/auth/components/LoginForm.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import Form from "@/src/shared/components/forms/Form"
+
+export default function LoginForm() {
+  return (
+    <Form>
+        <label className="block" htmlFor="email">E-mail</label>
+        <input type="email" id="email" placeholder="Ingresa tu email" className="border border-slate-200 w-full p-2"/>
+
+        <label className="block" htmlFor="password">Contraseña</label>
+        <input type="password" id="password" placeholder="Ingresa tu contraseña" className="border border-slate-200 w-full p-2"/>
+    </Form>
+  )
+}
