fa8e91ffccda3ca99ec3f0674616d3ad644754a1
LeoCiru <vasquezvolquezfelixleonel@gmail.com>
2026-08-13 10:30:04 -0400

    Crear Service y Repository para la autenticación

--- src/features/auth/actions/auth-actions.ts
+++ src/features/auth/actions/auth-actions.ts
@@ -1,7 +1,19 @@
 "use server"
 
-import { SignUpInput } from "../schemas/authSchema";
+import { SignUpInput, SignUpSchema } from "../schemas/authSchema";
 
 export async function signUpAction(input : SignUpInput) {
-    console.log(input);
+    const data = SignUpSchema.safeParse(input);
+
+    console.log(data.success);
+    
+    if (!data.success) {
+        return {
+            error: "Hubo un error",
+            sucess: "",
+        }
+    }
+
+    console.log(data.data);
+    
 }
