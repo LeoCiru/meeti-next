b2afc91cf2cb16a09be475088f6698e99d4f2ae2
LeoCiru <vasquezvolquezfelixleonel@gmail.com>
2026-08-13 10:37:45 -0400

    Enviar datos desde el Action hacia el Service

--- src/features/auth/actions/auth-actions.ts
+++ src/features/auth/actions/auth-actions.ts
@@ -1,6 +1,7 @@
 "use server"
 
 import { SignUpInput, SignUpSchema } from "../schemas/authSchema";
+import { authService } from "../services/AuthService";
 
 export async function signUpAction(input : SignUpInput) {
     const data = SignUpSchema.safeParse(input);
@@ -14,6 +15,6 @@ export async function signUpAction(input : SignUpInput) {
         }
     }
 
-    console.log(data.data);
+    await authService.register(data.data);
     
 }
--- src/features/auth/services/AuthService.ts
+++ src/features/auth/services/AuthService.ts
@@ -1,5 +1,18 @@
+import { SignUpInput } from "../schemas/authSchema";
+
 class AuthService {
 
+    async register(credentials : SignUpInput) {
+        const { email, name, password } = credentials;
+        
+        // Revisar si el usuario existe
+
+
+        // Validación de negocio
+
+
+        // Manejar el registro
+    }
 }
 
 export const authService = new AuthService();
